ea99ce316bc4dd6913f212e48813766f0fa1d5ed
Iris van Dijken <irisvdijken@gmail.com>
2020-02-25 11:23:21 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cars
+  get "/dashboard/request", to: "dashboard#request"
+  get "/dashboard/my_request", to: "dashboard#my_request"
 end
