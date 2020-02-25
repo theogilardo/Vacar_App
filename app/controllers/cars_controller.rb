baebceee6637dc7f2a20aa25b9fb40af44fefa09
theogilardo <gilardotheo@gmail.com>
2020-02-25 11:52:58 +0100

    Cars controller created + NEW view.template

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,49 @@
+class CarsController < ApplicationController
+
+  def index
+  @cars = Car.all
+  end
+
+  def show
+    @car = Car.find(params[:id])
+  end
+
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to cars_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @car = Car.find(params[:id])
+  end
+
+  def update
+    @car = Car.find(params[:id])
+    @car.update(car_params)
+    @car.save
+    redirect_to car_path(@car)
+  end
+
+  def destroy
+    @car = Car.find(params[:id])
+    @car.destroy
+    redirect_to cars_path
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:brand, :model, :number_of_seats, :registration_number, :transmission, :airconditioning, :year)
+  end
+end
+
+# No user_id in the params YET !!
+# No AVAILABILITY for Cars in the params YET !!
