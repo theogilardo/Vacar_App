7398ccdef9fff6ac40589e790add10d2b0e3bc55
theogilardo <gilardotheo@gmail.com>
2020-02-25 15:12:28 +0100

    Validation presence true :photo

--- app/models/car.rb
+++ app/models/car.rb
@@ -3,6 +3,7 @@ class Car < ApplicationRecord
   has_many :requests
   has_one_attached :photo
 
+  validates :photo, presence: true
   validates :brand, presence: true
   validates :number_of_seats, presence: true
   validates :registration_number, presence: true, uniqueness: true
