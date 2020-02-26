3b33e2c64437c05eeb09dc9e8a8ada7b13be1877
Tereza Valerova <terezavalerova@gmail.com>
2020-02-26 12:58:09 +0100

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,46 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+require 'faker'
+puts "Cleaning the database..."
+User.destroy_all
+
+puts "Creating users..."
+
+User.create!([
+  { email: 'maxim@gmail.com',
+    password: 'kjguo80438dg',
+    first_name: 'Maxim',
+    last_name: 'van Hoorn'
+  },
+  { email: 'iris@gmail.com',
+    password: 'jnfbodi498y34',
+    first_name: 'Iris',
+    last_name: 'van Dijken'
+  },
+])
+
+puts "You have #{User.count} users"
+
+puts "Creating cars..."
+
+Car.destroy_all
+10.times do
+  car = Car.new(
+    user: User.all.sample,
+    brand: Faker::Vehicle.make,
+    model: Faker::Vehicle.model,
+    year: rand(2000..2020),
+    price: rand(50..500),
+    number_of_seats: rand(2..8),
+    registration_number: Faker::Vehicle.license_plate,
+    transmission: ['manual', 'automatic'].sample
+  )
+  # photo = ['carphotos/orange.jpg', 'carphotos/white.jpg', 'carphotos/yellow.jpg', 'carphotos/grey.jpg', 'carphotos/red.jpg']
+  # car.photo.attach(io: file, filename: `#{photo.sample}`, content_type: 'image/jpg')
+
+  car.save
+end
+
+puts "You have #{Car.count} cars"
 
