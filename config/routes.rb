2aded730e96b43281a16e5e4b4cef35dda788462
Tereza Valerova <terezavalerova@gmail.com>
2020-02-26 10:58:48 +0100

    routes changed, reviews nested under requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cars do
-    
+  resources :cars
+
+
+  resources :requests do
     resources :reviews
   end
-  resources :requests
   get "/dashboard/request", to: "dashboard#request"
   get "/dashboard/my_request", to: "dashboard#my_request"
 end
